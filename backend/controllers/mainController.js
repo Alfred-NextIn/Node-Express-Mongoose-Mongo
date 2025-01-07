bd3b3aff0c38b8ad9427f953e75a470193543d77
Alfred-NextIn <acheampongalfredowusu2955@gmail.com>
2025-01-07 01:08:40 +0000

    Advanced Route protection and user personalisation after auth

--- backend/controllers/mainController.js
+++ backend/controllers/mainController.js
@@ -1,11 +1,13 @@
 import asyncHandler from 'express-async-handler';
 import Goal from '../models/goalModel.js';
+import User from '../models/userModel.js';
+
 
 //@desc   GET goals
 //@route  api/goals
 //access  PRIVATE
 export const getGoals = asyncHandler(async (req,res) => {
-    const goals = await Goal.find();
+    const goals = await Goal.find({user: req.user.id});
 
     res.status(200).json(goals);
 })
@@ -29,6 +31,7 @@ export const setGoal = async (req,res,next) => {
     }
 
     const goal = await Goal.create({
+        user: req.user.id,
         text: req.body.goal
     })
     res.status(201).json(goal);
@@ -44,11 +47,20 @@ export const updateGoal = async (req,res) => {
 
     const goal = await Goal.findById(id);
 
+    const user = await User.findById(req.user.id);
+
     if(!goal)
     {
-        res.status(400);
-        throw new Error('Goal Not Set');
+        res.status(400)
+        throw new Error("Goal Not Set");
     }
+
+    if(goal.user.toString() != user.id)
+    {
+        res.status(401)
+        throw new Error("You cannot update a goal that is not yours");
+    }
+
 
     const updatedGoal = await Goal.findByIdAndUpdate(id, { text: req.body.goal}, {
         new: true
@@ -69,6 +81,14 @@ export const deleteGoal = async (req,res) => {
     {
         res.status(400);
         throw new Error(`Goal with id: ${id} not found`);
+    }
+
+    const user = await User.findById(req.user.id);
+
+    if(goal.user.toString() != user.id)
+    {
+        res.status(401)
+        throw new Error("You cannot delete a goal that is not yours");
     }
 
     await goal.deleteOne();
--- backend/routes/mainRoute.js
+++ backend/routes/mainRoute.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { getGoals, getGoal, setGoal, updateGoal, deleteGoal } from '../controllers/mainController.js';
+import protect from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
@@ -11,9 +12,9 @@ const router = express.Router();
 
 //more improved way of joining routes without having to define each separately
 
-router.route('/:id').get(getGoal).put(updateGoal).delete(deleteGoal);
+router.route('/:id').get(protect, getGoal).put(protect, updateGoal).delete(protect, deleteGoal);
 
-router.route('/').get(getGoals).post(setGoal);
+router.route('/').get(protect,getGoals).post(protect,setGoal);
 
 
 export default router;
